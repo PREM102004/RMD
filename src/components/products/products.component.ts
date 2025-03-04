8bc463f647ed2715a1c7731c8c112d230018a5bc
Prem Kumar <premkumar.s@blindmatrix.com>
2025-03-04 15:22:09 +0530

    dsds

--- src/components/products/products.component.ts
+++ src/components/products/products.component.ts
@@ -82,7 +82,7 @@ getfilterdata(){
   //   this.showDangerAlert=true;
   //   setTimeout(() => this.showDangerAlert = false, 3000);
   //  }
-  if (this.filterschemes.length > 0) {
+  if (this.filterschemes.length != 0) {
     this.toastr.success('Schemes found successfully!', 'Success');
   } else if (this.filterschemes.length===0){
     this.toastr.error('No matching schemes found. Please try different inputs.', 'Error');
@@ -116,3 +116,21 @@ getfilterdata(){
 
     keepOriginalOrder = () => 0;
 }
+
+
+// <script defer src="/__/firebase/11.4.0/firebase-app-compat.js"></script>
+// <!-- include only the Firebase features as you need -->
+// <script defer src="/__/firebase/11.4.0/firebase-auth-compat.js"></script>
+// <script defer src="/__/firebase/11.4.0/firebase-database-compat.js"></script>
+// <script defer src="/__/firebase/11.4.0/firebase-firestore-compat.js"></script>
+// <script defer src="/__/firebase/11.4.0/firebase-functions-compat.js"></script>
+// <script defer src="/__/firebase/11.4.0/firebase-messaging-compat.js"></script>
+// <script defer src="/__/firebase/11.4.0/firebase-storage-compat.js"></script>
+// <script defer src="/__/firebase/11.4.0/firebase-analytics-compat.js"></script>
+// <script defer src="/__/firebase/11.4.0/firebase-remote-config-compat.js"></script>
+// <script defer src="/__/firebase/11.4.0/firebase-performance-compat.js"></script>
+// <!-- 
+//   initialize the SDK after all desired features are loaded, set useEmulator to false
+//   to avoid connecting the SDK to running emulators.
+// -->
+// <script defer src="/__/firebase/init.js?useEmulator=true"></script>

c409d29742bda1f86cbce5ea6e1d446ca235e2dd
Prem Kumar <premkumar.s@blindmatrix.com>
2025-03-04 14:12:04 +0530

    latest

--- src/components/products/products.component.ts
+++ src/components/products/products.component.ts
@@ -1,17 +1,25 @@
 import { CommonModule } from '@angular/common';
 import { HttpClient } from '@angular/common/http';
-import { Component, inject } from '@angular/core';
+import { ChangeDetectorRef, Component, inject } from '@angular/core';
 import { AccordionModule } from 'ngx-bootstrap/accordion';
 import { FormsModule } from '@angular/forms';
 import { AlertModule } from 'ngx-bootstrap/alert';
 import { ToastrService } from 'ngx-toastr';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-products',
   standalone: true,
-  imports: [CommonModule, AccordionModule,FormsModule,AlertModule ],
+  imports: [CommonModule, AccordionModule,FormsModule,AlertModule  ],
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
+  animations :[trigger('fadeIn', [
+    transition(':enter', [
+      style({ opacity: 0, transform: 'translateY(-20px)' }),
+      animate('400ms ease-in-out', style({ opacity: 1, transform: 'translateY(0)' }))
+    ])
+  ])]
 })
 export class ProductsComponent {
      productList:{ [key: string]: any[] }={};
@@ -25,19 +33,18 @@ export class ProductsComponent {
    occulistpation : string[]=[]
    selectedOccupation: string = '';
    showDangerAlert = false;
-
-    constructor(private http : HttpClient){}
+   private toastr = inject(ToastrService);
+    constructor(private http : HttpClient, private cd : ChangeDetectorRef){}
    
     ngOnInit():void{
       this.http.get<any>('assets/products.json').subscribe(response =>{
         this.datascheme = response.data;
-        console.log('this.productList: ', this.datascheme);
         this.occulistpation =this.datascheme.map((occ) => occ.Occupation);
         this.filteredProductList = { ...this.productList };
-        console.log('occulistpation',this.occulistpation)
+        this.cd.markForCheck()
       })
     }
-    private toastr = inject(ToastrService);
+   
     onSubmit(schemeForm: any) {
       if (schemeForm.invalid) {
         return;
